576b30b21830e80557d2408014ed9c49e8ce2d98
Ionésio Junior <ionesiojr@gmail.com>
2020-05-08 16:33:29 -0300

    Revert "ADD cpu/mem info"
    This reverts commit 6d02a30536688de1d40c96736292ed87018710f1.

--- grid_routes.py
+++ grid_routes.py
@@ -1,6 +1,5 @@
 from codes import MSG_FIELD, GRID_EVENTS
 import json
-import psutil
 
 import syft as sy
 
@@ -9,8 +8,6 @@ def _monitor(message: dict, conn_handler):
     response = {MSG_FIELD.TYPE: GRID_EVENTS.MONITOR_ANSWER}
 
     response[MSG_FIELD.NODES] = conn_handler.nodes
-    response[MSG_FIELD.CPU] = psutil.cpu_percent()
-    response[MSG_FIELD.MEM_USAGE] = psutil.virtual_memory().percent
     models = {
         model_id: model.json() for model_id, model in conn_handler.worker.models.items()
     }
